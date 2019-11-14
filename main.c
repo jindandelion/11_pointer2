fb74875f1ddf36352b6c20e76dd8221f9a671d43
jindandelion <gieoun524@sookmyung.ac.kr>
2019-11-14 15:37:02 +0900

    Practice5

--- main.c
+++ main.c
@@ -2,18 +2,17 @@
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
+char *proverb="All that glisters is not gold.";
+void setPrinter(char **q)
+{
+	*q=proverb;
+}
 
 int main(void) 
 {
-	int i=100;
-	int *p=&i;
-	int **q=&p;
+	char *p="zzz";
+	setPrinter(&p);
+	printf("%s\n",p);
 	
-	*p=200;
-	printf("i=%d,*p=%d,**q=%d\n",i,*p,**q);
-	
-	**q=300;
-	printf("i=%d,*p=%d,**q=%d\n",i,*p,**q);
-	
-	return 0;	
+	return 0;
 }
